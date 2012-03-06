398775938dffa83d8d99a2766202f71bf8aa7589
Tecnologic86 <tecnologic86@gmail.com>
2012-03-06 17:17:13 +0100

    Updated Filehandler tast to play mp3 file
    Change-Id: I5a04bee1e64c2ba0157c2dac0e079218d559df7e

--- Fatfs/CFileHandler.cpp
+++ Fatfs/CFileHandler.cpp
@@ -20,34 +20,15 @@
 #include "mp3dec.h"
 #include "stm32f4_discovery_audio_codec.h"
 /* Private typedef -----------------------------------------------------------*/
-/*******************************
- * These are possible application
- * states.
- ******************************/
-typedef enum
-{
-    IDLE_STATE,
-    DEVICE_ATTACHED,
-    SEARCH_MP3_FILES,
-    OPEN_MP3_FILE,
-    FILE_OPEN,
-    FRAME_SYNC_FIND,
-    FILE_READ,
-    MP3_DECODE,
-    PLAYBACK,
-    FILE_CLOSE,
-    PLAYBACK_PAUSE,
-    PLAYBACK_STOP,
-    SYSTEM_HALT
 
-}APPLICATION_STATE;
 /* Private define ------------------------------------------------------------*/
-#define BUFFER_SIZE		4096
+#define READBUF_SIZE	1940
+BYTE    readBuf [READBUF_SIZE];
+BYTE   *readPtr;
+int   outBuf  [READBUF_SIZE];
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-APPLICATION_STATE applicationState,previousAppState;
 /* Private function prototypes -----------------------------------------------*/
-
 /* Private functions ---------------------------------------------------------*/
 
 /**
@@ -88,6 +69,8 @@ static int FillReadBuffer(unsigned char *readBuf, unsigned char *readPtr, int bu
 CFileHandler::CFileHandler() {
 	Status = SD_OK;
 	fsresult = FR_OK;
+	applicationState = IDLE_STATE;
+	previousAppState = IDLE_STATE;
 }
 
 CFileHandler::~CFileHandler() {
@@ -111,9 +94,10 @@ bool CFileHandler::HardwareInit(){
   */
 void CFileHandler::Run(){
 
-	uint8_t uiReadBuffer[BUFFER_SIZE];
-	unsigned int uiBytesRead=0;
 	HMP3Decoder mp3Decoder;
+	MP3FrameInfo mp3FrameInfo;
+	unsigned int bytesLeft;
+	int nRead, offset, eofReached, err;
 
 	/*-----------------------------------------------------------------------------
 	Initialize the MP3decoder structure
@@ -124,64 +108,248 @@ void CFileHandler::Run(){
 		while(1);
 	}
 
-	/*-----------------------------------------------------------------------------
-	Initialize the audio dac and codec
-	-----------------------------------------------------------------------------*/
-	EVAL_AUDIO_Init(OUTPUT_DEVICE_AUTO,100,44100);
+	EVAL_AUDIO_SetAudioInterface(AUDIO_INTERFACE_I2S);
 
+	while(1){
 	/*-----------------------------------------------------------------------------
-	Beginning of FAT file system related code.  The following code shows steps
-	necessary to create, read, and write files.
-	See http://elm-chan.org/fsw/ff/00index_e.html for more information.
-	This code assumes a single logical drive, drive number 0.  It also assumes a single partition.
+	Statemachine
 	-----------------------------------------------------------------------------*/
+		switch(applicationState)
+		{
+		case(IDLE_STATE):
+			/* not implemented yet */
+			applicationState = OPEN_MP3_FILE;
 
-	/*-----------------------------------------------------------------------------
-	Mount the file system on logical drive 0.  Mounting associates the file system
-	structure with a logical drive number.  Mounting must be done before any file
-	operations.  Mounting does not write anything to the SD card, it simply
-	initializes and associates the file structure.  The file system structure
-	members are not filled in until the first file operation after f_mount.
-	-----------------------------------------------------------------------------*/
-	fsresult = f_mount(0, &myfs);
+			break;
 
-	if (fsresult != FR_OK){
-		//FAT file system mounting failed. FRESULT Error code: %d.  See FATfs/ff.h for FRESULT code meaning.
-		while(1);
+/*------------------------------------------------------------------------------*/
+		case(OPEN_MP3_FILE):
+			/*-----------------------------------------------------------------------------
+			Mount the file system on logical drive 0.  Mounting associates the file system
+			structure with a logical drive number.  Mounting must be done before any file
+			operations.  Mounting does not write anything to the SD card, it simply
+			initializes and associates the file structure.  The file system structure
+			members are not filled in until the first file operation after f_mount.
+			-----------------------------------------------------------------------------*/
+			fsresult = f_mount(0, &myfs);
+
+			if (fsresult != FR_OK){
+				//FAT file system mounting failed. FRESULT Error code: %d.  See FATfs/ff.h for FRESULT code meaning.
+				while(1);
+			}
+
+			/*-----------------------------------------------------------------------------
+			Open a file
+			-----------------------------------------------------------------------------*/
+			fsresult = f_open(&myfile, "A-Team.mp3", FA_OPEN_EXISTING | FA_READ);  //open file for read
+			if (fsresult != FR_OK){
+				//File open failed. FRESULT Error code: %d.  See FATfs/ff.h for FRESULT code meaning
+				while(1);
+			}
+			f_sync(&myfile);   // minimize critical section, see Appnote from fatfs
+
+			applicationState = FILE_OPEN ;  //File is open change state
+
+			break;
+
+/*------------------------------------------------------------------------------*/
+		case(FILE_OPEN):
+
+			/* Intialize the MP3 decoder variables
+			 * as we will start decoding a new file.
+			 * The details of the MP3 file are obtained
+			 * and the read pointer is set up to point
+			 * to the start of the MP3 frame.*/
+
+			/* At this point, the MP3 decoder is already
+			 * initialized. Reset all the state variables */
+
+			bytesLeft = 0;
+			eofReached = 0;
+			readPtr = readBuf;
+			nRead = 0;
+			applicationState = FRAME_SYNC_FIND;
+			break;
+
+
+/*------------------------------------------------------------------------------*/
+		case(FRAME_SYNC_FIND):
+
+
+			/* Read the mp3 file. Fill the first buffer of
+			 * data from the input file. */
+
+			fsresult = f_read(&myfile,readBuf,READBUF_SIZE,&bytesLeft);
+			if (fsresult != FR_OK){
+				//File read failed. FRESULT Error code: %d.  See FATfs/ff.h for FRESULT code meaning
+				while(1);
+			}
+
+			/* If end of file is reached then close this file */
+			if(bytesLeft == 0)
+			{
+				applicationState = FILE_CLOSE;
+				break;
+			}
+
+			do {
+			/* find start of first MP3 frame - assume
+			 * EOF if no sync found */
+
+			offset = MP3FindSyncWord(readPtr, bytesLeft);
+			if (offset < 0)
+			{
+				/* This means this frame does not contain
+				 * the frame sync. Get another frame. */
+
+				readPtr = readBuf;
+				applicationState = FRAME_SYNC_FIND;
+				break;
+
+			}
+
+			/* Update the read pointer and number of bytes
+			 * left in the current frame. */
+
+			readPtr += offset;
+			bytesLeft -= offset;
+
+			/* Determine the audio format from the first frame header
+			 * The information in mp3FrameInfo can be displayed on the
+			 * display. */
+			err = MP3GetNextFrameInfo(mp3Decoder, &mp3FrameInfo, readPtr);
+			} while(err < 0); // as long as a valid frame was found
+			// should run only once in most cases, but when ID3 tag
+			// contains false SyncWord, this wil prevent error
+
+
+			if( mp3FrameInfo.nChans != 2)
+			{
+				/* This means that the MP3 file is  a
+				 * mono audio stream
+				 * We currently dont want to handle these type
+				 * of files even if the decoder can decode
+				 * them */
+				applicationState = FILE_CLOSE;
+				break;
+			}
+
+			/*-----------------------------------------------------------------------------
+			Initialize the audio dac and codec, with sample rate from Mp3 file
+			-----------------------------------------------------------------------------*/
+			EVAL_AUDIO_DeInit();
+			EVAL_AUDIO_Init(OUTPUT_DEVICE_AUTO,100,mp3FrameInfo.samprate);
+
+			/* Start reading the MP3 file.*/
+			applicationState = FILE_READ;
+			break;
+
+/*------------------------------------------------------------------------------*/
+		case(FILE_READ):
+
+			/* Read some more data is the number of bytes
+			 * left is less than a frame size and we have
+			 * not reached the end of file.
+			 */
+
+			if (bytesLeft < READBUF_SIZE && !eofReached)
+			{
+				nRead = FillReadBuffer(readBuf, readPtr, READBUF_SIZE, bytesLeft, &myfile);
+				bytesLeft += nRead;
+				readPtr = readBuf;
+				if (nRead == 0)
+					eofReached = 1;
+			}
+
+			/* Decode the frame */
+			applicationState = MP3_DECODE;
+
+
+			break;
+
+/*------------------------------------------------------------------------------*/
+		case(MP3_DECODE):
+		/*-----------------------------------------------------------------------------
+		Decode mp3 file
+		-----------------------------------------------------------------------------*/
+
+			err = MP3Decode(mp3Decoder, &readPtr, (int*)&bytesLeft,(short int*) outBuf, 0);
+
+			applicationState = PLAYBACK;
+			if (err)
+			{
+				/* error occurred */
+				switch (err)
+				{
+				case -1:
+					applicationState = FILE_CLOSE;
+					break;
+				case -2:
+					/* More data is needed. Read the file. */
+					applicationState = FILE_READ;
+					break;
+				case -3:
+					applicationState = FILE_CLOSE;
+					break;
+				case -6:
+					applicationState = FRAME_SYNC_FIND;
+					break;
+				default:
+					applicationState = FILE_CLOSE;
+					break;
+				}
+			}
+
+			break;
+
+/*------------------------------------------------------------------------------*/
+		case(PLAYBACK):
+	            		{
+			/* In this state an stereo audio frame is
+			 * available to play. Play back the frame
+			 * and then read the file for more
+			 * data */
+
+			EVAL_AUDIO_Play((uint16_t*)outBuf,(long int)sizeof(outBuf));
+
+			applicationState = FILE_READ;
+			break;
+
+
+	            		}
+		case(PLAYBACK_PAUSE):
+	            				break;
+
+
+		case (PLAYBACK_STOP):
+	            				break;
+
+		case (SYSTEM_HALT):
+	            				break;
+
+		case(FILE_CLOSE):
+	            		{
+			/* The application would have reached here if
+			 * there is an error opening the file or the
+			 * playback is done. In either case try the
+			 * next file in the list
+			 */
+
+			EVAL_AUDIO_DeInit();
+			f_close(&myfile);
+			applicationState = OPEN_MP3_FILE;
+			break;
+	            		}
+		default:
+			while(1);
+			break;
+		}
 	}
 
-	/*-----------------------------------------------------------------------------
-	Open a file and read first buffer
-	-----------------------------------------------------------------------------*/
-	fsresult = f_open(&myfile, "A-Team.mp3", FA_OPEN_EXISTING | FA_READ);  //open file for read
-	if (fsresult != FR_OK){
-		//File open failed. FRESULT Error code: %d.  See FATfs/ff.h for FRESULT code meaning
-		while(1);
-	}
-	f_sync(&myfile);   // minimize critical section, see Appnote from fatfs
-
-	/*-----------------------------------------------------------------------------
-	read buffer out of file
-	-----------------------------------------------------------------------------*/
-	fsresult = f_read(&myfile,uiReadBuffer,BUFFER_SIZE,&uiBytesRead);
-	if (fsresult != FR_OK){
-		//File read failed. FRESULT Error code: %d.  See FATfs/ff.h for FRESULT code meaning
-		while(1);
-	}
-	/*-----------------------------------------------------------------------------
-	Decode Mp3 file, to end of file
-	-----------------------------------------------------------------------------*/
-
-
-
-
-
-
-	/* Infinite loop */
-	while (1)
-	{}
 }
+
 }
+
 
 
 
--- Fatfs/CFileHandler.h
+++ Fatfs/CFileHandler.h
@@ -16,6 +16,25 @@
 #include "stm32f4_sdio.h"
 #include "ff.h"
 /* Exported types ------------------------------------------------------------*/
+/*******************************
+ * These are possible application
+ * states.
+ ******************************/
+typedef enum
+{
+    IDLE_STATE,
+    OPEN_MP3_FILE,
+    FILE_OPEN,
+    FRAME_SYNC_FIND,
+    FILE_READ,
+    MP3_DECODE,
+    PLAYBACK,
+    FILE_CLOSE,
+    PLAYBACK_PAUSE,
+    PLAYBACK_STOP,
+    SYSTEM_HALT
+
+}APPLICATION_STATE;
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
@@ -29,6 +48,7 @@ class CFileHandler: public AManagedTask{
 private:
 	SD_Error Status;
 	SD_CardInfo SDCardInfo;
+	APPLICATION_STATE applicationState,previousAppState;
 
 	FRESULT fsresult;               //return code for file related operations
 	FATFS myfs;                     //FAT file system structure, see ff.h for details
