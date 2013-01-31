60884273d36852e225796c2b17e43766c125357a
Tecnologic86 <tecnologic86@gmail.com>
2013-01-31 21:58:39 +0100

    initial commit of basic GUI elements
    Change-Id: I37bdb6f6fcd2980039d3a3a2024da185b5ea0ddf

---
+++ GUI/CGUIAlarmSubPage.cpp
@@ -0,0 +1,21 @@
+/*
+ * CGUIAlarmSubPage.cpp
+ *
+ *  Created on: 05.09.2012
+ *      Author: brand
+ */
+
+#include "CGUIAlarmSubPage.h"
+
+namespace ThunderCryerGUI {
+
+  CGUIAlarmSubPage::CGUIAlarmSubPage() {
+    // TODO Auto-generated constructor stub
+
+  }
+
+  CGUIAlarmSubPage::~CGUIAlarmSubPage() {
+    // TODO Auto-generated destructor stub
+  }
+
+} /* namespace ThunderCryerGUI */
---
+++ GUI/CGUIAlarmSubPage.h
@@ -0,0 +1,22 @@
+/*
+ * CGUIAlarmSubPage.h
+ *
+ *  Created on: 05.09.2012
+ *      Author: brand
+ */
+
+#ifndef CGUIALARMSUBPAGE_H_
+#define CGUIALARMSUBPAGE_H_
+
+#include "CGUIPage.h"
+
+namespace ThunderCryerGUI {
+
+  class CGUIAlarmSubPage: public ThunderCryerGUI::CGUIPage {
+  public:
+    CGUIAlarmSubPage();
+    virtual ~CGUIAlarmSubPage();
+  };
+
+} /* namespace ThunderCryerGUI */
+#endif /* CGUIALARMSUBPAGE_H_ */
