d889483e98ea0e2af139e34be31fa9700c974f44
Tecnologic86 <tecnologic86@gmail.com>
2013-02-06 00:37:32 +0100

    cleaned main of freertos stuff

--- main.cpp
+++ main.cpp
@@ -9,35 +9,14 @@
   */
 
 /* Includes ------------------------------------------------------------------*/
-#include "CFreeRTOS.h"
-#include "CTask.h"
-#include "ustime.h"
-#include "CLedHeartBeatSTM32F4Disc.h"
-#include "CMenue.h"
-#include "CTime.h"
-#include "CT6963GPIOInterface.h"
-#include "CUSBMassStorage.h"
-#include "CFileHandler.h"
-#include "stm32f4xx_conf.h"
-#include "stm32f4xx.h"
+#include "ch.hpp"
+#include "hal.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-CLedHeartBeatSTM32F4Disc g_LedTast(200/portTICK_RATE_MS);
-CT6963_GPIO_Interface g_Display;
-CMenue	g_Menue(&g_Display);
-CFileHandler g_FileHandler;
-CUSB_MassStorage g_MSC;
-
 /* Private function prototypes -----------------------------------------------*/
-extern "C" {
-	void vApplicationTickHook( void );
-	void vApplicationIdleHook( void );
-	void vApplicationMallocFailedHook( void );
-	void vApplicationStackOverflowHook( xTaskHandle *pxTask, signed char *pcTaskName );
-} // extern "C"
 /* Private functions ---------------------------------------------------------*/
 
 
@@ -50,26 +29,7 @@ extern "C" {
 int main(void){
 	unsigned int u;
 
-	/**
-	 * Unit Tests
-	 */
-	Time::CTimeUnitTest();
 
-	/**
-	 * create Led Heartbeat task with minimal stack size and priority
-	 * 1 higher than idle task
-	 */
-	g_LedTast.Create("Led Heartbeat",configMINIMAL_STACK_SIZE,tskIDLE_PRIORITY+3);
-	g_MSC.Create("USB Massstorage", configMINIMAL_STACK_SIZE * 10, tskIDLE_PRIORITY +1);
-	g_Menue.Create("Menue",configMINIMAL_STACK_SIZE * 2,tskIDLE_PRIORITY+3);
-	g_FileHandler.Create("FileHandler",configMINIMAL_STACK_SIZE * 20,tskIDLE_PRIORITY+1);
-
-	/**
-	 * Initialize the hardware for all Tasks and
-	 * then FreeRTOS takes control of execution
-	 */
-	CFreeRTOS::InitHardwareForManagedTasks();
-	CFreeRTOS::StartScheduler();
 
 	while(1){
 		u++;
@@ -78,34 +38,3 @@ int main(void){
 	return 0;
 
 }
-
-extern "C" {
-
-	// This FreeRTOS callback function gets called once per tick (default = 1000Hz).
-	// ----------------------------------------------------------------------------
-	void vApplicationTickHook( void ) {
-
-	}
-
-	// This FreeRTOS call-back function gets when no other task is ready to execute.
-	// On a completely unloaded system this is getting called at over 2.5MHz!
-	// ----------------------------------------------------------------------------
-	void vApplicationIdleHook( void ) {
-
-	}
-
-	// A required FreeRTOS function.
-	// ----------------------------------------------------------------------------
-	void vApplicationMallocFailedHook( void ) {
-		configASSERT( 0 );  // Latch on any failure / error.
-	}
-
-	// A required FreeRTOS function. gets called on stack overflow
-	// ----------------------------------------------------------------------------
-	void vApplicationStackOverflowHook( xTaskHandle *pxTask, signed char *pcTaskName ){
-		/* TODO, implement your code here */
-		while(1);
-	}
-} // extern "C"
-
-}//Somewhere is a { open i think i left one #ifdef __cplusplus  closing brance
