ffab2797bdecc7e621ffd7d59255026a52abe9e6
Alexander Brand <Tecnologic86@gmail.com>
2013-03-05 23:18:09 +0100

    updated pinning for lcd
    according to Mainboard rev V5

--- GraphicLCD/CT6963GPIOInterface.cpp
+++ GraphicLCD/CT6963GPIOInterface.cpp
@@ -11,7 +11,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "CT6963GPIOInterface.h"
 #include "FreeRTOS.h"
-#include "task.h"
+#include "CTask.h"
 #include "ustime.h"
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
@@ -28,24 +28,25 @@
 #define GLCD_DATA_OUTPUT		GLCD_DATA_PORT->MODER |= GLCD_DATA_MODE_MASK_OUT;
 
 // control port
-#define GLCD_CTRL_PORT_CD_RD	GPIOC
-#define GLCD_CTRL_PORT_WR_CE	GPIOB
+#define GLCD_CTRL_PORT          GPIOB
 
 // control signals
-#define GLCD_WR			GPIO_Pin_0
-#define GLCD_RD			GPIO_Pin_5
-#define GLCD_CE			GPIO_Pin_1
-#define GLCD_CD			GPIO_Pin_4
-#define GLCD_RESET		GPIO_Pin_11
+#define GLCD_RD			GPIO_Pin_11
+#define GLCD_WR			GPIO_Pin_12
+#define GLCD_RESET		GPIO_Pin_13
+#define GLCD_CE			GPIO_Pin_14
+#define GLCD_CD			GPIO_Pin_15
+
 
 // aux pins
 #define GLCD_CONTRAST_PORT 	GPIOB
 #define GLCD_CONTRAST_MASK 	(GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2)
+#define GLCD_LED                GPIO_Pin_7
 
-#define GLCD_AUX_PORT		GPIOA
-#define GLCD_LED			GPIO_Pin_3
-#define GLCD_BACKLIGHT		GPIO_Pin_1
-#define GLCD_BUZZER			GPIO_Pin_2
+#define GLCD_AUX_PORT		GPIOE
+
+#define GLCD_BACKLIGHT		GPIO_Pin_5
+#define GLCD_BUZZER		GPIO_Pin_6
 
 
 const unsigned long c_iDelayFore = 2;
@@ -78,15 +79,13 @@ bool CheckStatus(void)
 	uint16_t tmp;
 	GLCD_DATA_INPUT;
 
-	GPIO_ResetBits(GLCD_CTRL_PORT_CD_RD , GLCD_RD);
-	GPIO_ResetBits(GLCD_CTRL_PORT_WR_CE , GLCD_CE);
+	GPIO_ResetBits(GLCD_CTRL_PORT, GLCD_RD | GLCD_CE);
 
 	delay_us(c_iDelayFore);
 
 	tmp = ((GPIO_ReadInputData(GLCD_DATA_PORT) & GLCD_DATA_PORT_MASK) >> GLCD_DATA_OFFSET);
 
-	GPIO_SetBits(GLCD_CTRL_PORT_WR_CE , GLCD_CE);
-	GPIO_SetBits(GLCD_CTRL_PORT_CD_RD , GLCD_RD);
+	GPIO_SetBits(GLCD_CTRL_PORT,  GLCD_RD | GLCD_CE);
 
 	delay_us(c_iDelayAfter);
 
@@ -107,7 +106,7 @@ void CT6963_GPIO_Interface::HardwareInit(void){
 
 	GPIO_InitTypeDef GPIO_InitStructure;
 	// activate Clock for Io Ports used
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA , ENABLE);
+	//RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA , ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB , ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC , ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE , ENABLE);
@@ -119,11 +118,8 @@ void CT6963_GPIO_Interface::HardwareInit(void){
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
 
 	//Control pins CD and RD
-	GPIO_InitStructure.GPIO_Pin = GLCD_CD | GLCD_RD;
-	GPIO_Init(GLCD_CTRL_PORT_CD_RD,&GPIO_InitStructure);
-	//Control pins WR, CE and RST
-	GPIO_InitStructure.GPIO_Pin = GLCD_WR | GLCD_CE | GLCD_RESET;
-	GPIO_Init(GLCD_CTRL_PORT_WR_CE,&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GLCD_CD | GLCD_RD | GLCD_WR | GLCD_CE | GLCD_RESET;
+	GPIO_Init(GLCD_CTRL_PORT,&GPIO_InitStructure);
 
 	// data pins are high byte
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
@@ -131,7 +127,8 @@ void CT6963_GPIO_Interface::HardwareInit(void){
 	GPIO_Init(GLCD_DATA_PORT,&GPIO_InitStructure);
 
 	//Set all Control pins to high level
-	GPIO_SetBits(GLCD_CTRL_PORT_CD_RD , GLCD_CD | GLCD_RD);	GPIO_SetBits(GLCD_CTRL_PORT_WR_CE , GLCD_WR | GLCD_CE );
+	GPIO_SetBits(GLCD_CTRL_PORT, GLCD_CD | GLCD_RD | GLCD_WR | GLCD_CE);	
+
 
 
 	/*
@@ -141,16 +138,18 @@ void CT6963_GPIO_Interface::HardwareInit(void){
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
-	GPIO_InitStructure.GPIO_Pin = GLCD_CONTRAST_MASK;
+	GPIO_InitStructure.GPIO_Pin = GLCD_CONTRAST_MASK | GLCD_LED;
 	GPIO_Init(GLCD_CONTRAST_PORT,&GPIO_InitStructure);
+
 	// set contrast to 4 off 8
 	GPIO_SetBits(GLCD_CONTRAST_PORT, (GLCD_CONTRAST_MASK & 4));
+        GPIO_SetBits(GLCD_CONTRAST_PORT, GLCD_LED);
 
 	// set light pins
-	GPIO_InitStructure.GPIO_Pin = GLCD_LED | GLCD_BACKLIGHT | GLCD_BUZZER;
+	GPIO_InitStructure.GPIO_Pin = GLCD_BACKLIGHT | GLCD_BUZZER;
 	GPIO_Init(GLCD_AUX_PORT,&GPIO_InitStructure);
 	// "Festtagsbeleuchtung"
-	GPIO_SetBits(GLCD_AUX_PORT, GLCD_LED | GLCD_BACKLIGHT | GLCD_BUZZER);
+	GPIO_SetBits(GLCD_AUX_PORT, GLCD_BACKLIGHT | GLCD_BUZZER);
 
 	init_us_timer(); //delay timer init
 
@@ -163,13 +162,13 @@ void CT6963_GPIO_Interface::HardwareInit(void){
   */
 void CT6963_GPIO_Interface::Reset(void){
 
-	GPIO_ResetBits(GLCD_CTRL_PORT_WR_CE , GLCD_RESET | GLCD_CE );
+	GPIO_ResetBits(GLCD_CTRL_PORT, GLCD_RESET | GLCD_CE );
 	GPIO_SetBits(GLCD_AUX_PORT, GLCD_BUZZER);
 
-	vTaskDelay(20);  // sleep 20 ms
+	CTask::Delay(20);  // sleep 20 ms
 
 	GPIO_ResetBits(GLCD_AUX_PORT, GLCD_BUZZER);
-	GPIO_SetBits(GLCD_CTRL_PORT_WR_CE , GLCD_RESET | GLCD_CE );
+	GPIO_SetBits(GLCD_CTRL_PORT, GLCD_RESET | GLCD_CE );
 
 }
 
@@ -185,13 +184,11 @@ void CT6963_GPIO_Interface::WriteCommand(const unsigned char command){
 	GLCD_DATA_PORT->ODR &= ~GLCD_DATA_PORT_MASK;  //Clear Data pins
 	GLCD_DATA_PORT->ODR |= (command << (GLCD_DATA_OFFSET)); // lowbyte will stay as is in this write
 
-	GPIO_ResetBits(GLCD_CTRL_PORT_WR_CE , GLCD_WR);
-	GPIO_ResetBits(GLCD_CTRL_PORT_WR_CE , GLCD_CE);
+	GPIO_ResetBits(GLCD_CTRL_PORT, GLCD_WR | GLCD_CE);
 
 	delay_us(c_iDelayFore);   // time for display to read the data
 
-	GPIO_SetBits(GLCD_CTRL_PORT_WR_CE , GLCD_CE);
-	GPIO_SetBits(GLCD_CTRL_PORT_WR_CE , GLCD_WR);
+	GPIO_SetBits(GLCD_CTRL_PORT , GLCD_WR | GLCD_CE);
 
 
 	delay_us(c_iDelayAfter);
@@ -208,15 +205,11 @@ void CT6963_GPIO_Interface::WriteData(const unsigned char data){
 	GLCD_DATA_PORT->ODR &= ~GLCD_DATA_PORT_MASK;  //Clear Data pins
 	GLCD_DATA_PORT->ODR |= ((uint16_t)data << GLCD_DATA_OFFSET); // lowbyte will stay as is in this write
 
-	GPIO_ResetBits(GLCD_CTRL_PORT_CD_RD , GLCD_CD);
-	GPIO_ResetBits(GLCD_CTRL_PORT_WR_CE , GLCD_WR);
-	GPIO_ResetBits(GLCD_CTRL_PORT_WR_CE , GLCD_CE);
+	GPIO_ResetBits(GLCD_CTRL_PORT, GLCD_CD | GLCD_WR | GLCD_CE);
 
 	delay_us(c_iDelayFore);
 
-	GPIO_SetBits(GLCD_CTRL_PORT_WR_CE , GLCD_CE);
-	GPIO_SetBits(GLCD_CTRL_PORT_CD_RD , GLCD_CD);
-	GPIO_SetBits(GLCD_CTRL_PORT_WR_CE , GLCD_WR);
+	GPIO_SetBits(GLCD_CTRL_PORT, GLCD_CD | GLCD_WR | GLCD_CE);
 
 	delay_us(c_iDelayAfter);
 }
@@ -232,17 +225,13 @@ unsigned char CT6963_GPIO_Interface::ReadData(){
 
 	GLCD_DATA_INPUT;
 
-	GPIO_ResetBits(GLCD_CTRL_PORT_CD_RD , GLCD_RD );
-	GPIO_ResetBits(GLCD_CTRL_PORT_CD_RD , GLCD_CD );
-	GPIO_ResetBits(GLCD_CTRL_PORT_WR_CE , GLCD_CE );
+	GPIO_ResetBits(GLCD_CTRL_PORT, GLCD_RD | GLCD_CD | GLCD_CE);
 
 	delay_us(c_iDelayFore);
 
 	tmp = ((GPIO_ReadInputData(GLCD_DATA_PORT) & GLCD_DATA_PORT_MASK) >> GLCD_DATA_OFFSET);
 
-	GPIO_SetBits(GLCD_CTRL_PORT_WR_CE , GLCD_CE );
-	GPIO_SetBits(GLCD_CTRL_PORT_CD_RD , GLCD_CD );
-	GPIO_SetBits(GLCD_CTRL_PORT_CD_RD , GLCD_RD );
+	GPIO_SetBits(GLCD_CTRL_PORT, GLCD_RD | GLCD_CD | GLCD_CE );
 
 	delay_us(c_iDelayAfter);
 
