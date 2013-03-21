5b82315283eee446c9f4f888d63fca02c6a94196
Tecnologic <tecnologic86@gmail.com>
2013-03-21 16:04:13 +0100

    raised tickrate to 1000Hz

--- FreeRTOS/Source/include/FreeRTOSConfig.h
+++ FreeRTOS/Source/include/FreeRTOSConfig.h
@@ -75,7 +75,7 @@
 #define configUSE_IDLE_HOOK			0
 #define configUSE_TICK_HOOK			0
 #define configCPU_CLOCK_HZ			( SystemCoreClock )
-#define configTICK_RATE_HZ			( ( portTickType ) 100 )
+#define configTICK_RATE_HZ			( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 10 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 256 )
 #define configMAX_TASK_NAME_LEN		( 16 )
