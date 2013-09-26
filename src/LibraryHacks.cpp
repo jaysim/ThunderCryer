4db2d70b922a576d92db134646435393198d8bfb
Alexander Brand <Tecnologic86@gmail.com>
2013-09-26 23:50:05 +0200

    added lib hacks to prevent use of cpp lib functions adding code an unsupported functions

---
+++ src/LibraryHacks.cpp
@@ -0,0 +1,76 @@
+/**
+ ******************************************************************************
+ * @file      	LibraryHacks.cpp
+ * @author    	Tecnologic86
+ * @version   	V0.0.0
+ * @date      	03.07.2012 15:08:09
+ * @project	ThunderCryer
+ ******************************************************************************
+ */
+
+
+
+/*
+ * LibraryHacks.cpp  *
+ *  Created on: 23 Jan 2011  *
+ *  Author: Andy  */
+
+#include "ch.hpp"
+#include <cstdlib>
+//#include <cross_studio_io.h>
+
+/*
+ * The default pulls in 70K of garbage
+ */
+namespace __gnu_cxx {
+void __verbose_terminate_handler() {
+	for(;;);
+}
+}
+
+/*
+ * The default pulls in about 12K of garbage
+ */
+extern "C" void __cxa_pure_virtual() {
+	for(;;);
+}
+
+/*
+ * Implement C++ new/delete operators using the heap
+ */
+void *operator new(size_t size) {
+	return chCoreAlloc(size);
+}
+
+void *operator new[](size_t size) {
+	return chCoreAlloc(size);
+}
+
+void operator delete(void *p) {
+	free(p);
+}
+
+void operator delete[](void *p) {
+	free(p);
+}
+
+
+extern "C" {
+  void *__dso_handle = 0;
+
+/*! \brief User defined behaviour for the assert macro \ingroup Functions \synopsis
+
+ \desc There is no default implementation of \b \this. Keeping \b \this 
+  out of the library means that you can can customize its behaviour without rebuilding 
+  the library. You must implement this function where \a expression is the stringized
+  expression, \a filename is the filename of the source file and \a line
+  is the linenumber of the failed assertion.
+*/
+#if 0
+void __assert(const char *__expression, const char *__filename, int __line){
+
+	//debug_printf("%s in file: %s, at line: %s", __expression, __filename, __line);
+}
+#endif
+}
+
