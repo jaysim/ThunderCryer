9a51547b38dc2d0af8c9016a22a11aa86ec16be5
Alexander Brand <Tecnologic86@gmail.com>
2013-10-26 22:59:42 +0200

    added wifi exti callback and handler as globals

--- inc/main.h
+++ inc/main.h
@@ -13,12 +13,16 @@
 
 #include "CUSBVirtualCom.h"
 #include "CRTCHandler.h"
+#include "CWiFiHandler.h"
+#include "cc3000.h"
 
 
 extern chibios_rt::CUSBVirtualCom SDU2;
 extern chibios_rt::CRTCHander rtcHandlerThread;
+extern mbed_cc3000::CWiFiHandler wifiHandler;
 
 extern void extcbAlarm(EXTDriver *extp, expchannel_t channel);
 extern void extcbOneSec(EXTDriver *extp, expchannel_t channel);
+extern void extcbWifi(EXTDriver *extp, expchannel_t channel);
 
 #endif /* MAIN_H_ */
